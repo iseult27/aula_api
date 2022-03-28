bb58ba7e0cf3f65d660d9a2ee1c05ee2ae0fccb2
Iseult <isismgx@outlook.com>
2022-03-28 01:30:30 -0300

    api

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+
+const catButton = document.getElementById('change-cat');
+
+const getCats = async () => {
+
+    try {
+    const data = await fetch(BASE_URL);
+    const json = await data.json();
+
+    return json.webpurl;
+
+    }
+    catch (e) {
+
+        console.log(e.message);
+    }
+};
+
+const loadImg = async () => {
+    const catImg = document.getElementById('cat');
+    catImg.src = await getCats();
+}
+
+catButton.addEventListener('click', loadImg);
+
+loadImg();
